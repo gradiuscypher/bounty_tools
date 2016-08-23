f8a8d12a3a90962e6430cfb9ebf0843ace5ff8c5
gradiuscypher <gradiuscypher@gmail.com>
2016-08-23 09:06:01 -0700

    Adding a bit more work done as well as a requirements

--- es_bounty.py
+++ es_bounty.py
@@ -4,6 +4,9 @@
 import argparse
 import subprocess
 import traceback
+import sqlite3
+import elasticsearch
+from os.path import expanduser
 
 
 def export_to_sheets():
@@ -22,12 +25,24 @@ def import_from_reconng(workspace, remote=None):
     # copy file from local or SCP from remote host
     # iterate through all tables of DB and extract all useful information to proper workspace indexes
     print("Importing from recon-ng")
+    source_file = expanduser("~") + "/.recon-ng/workspaces/{}/data.db".format(workspace)
+
     if remote is not None:
         try:
             print("Remote importing from", remote)
-            source_file = "~/.recon-ng/workspaces/{}".format(workspace)
         except:
             print("An error occured while trying to SCP the file from the remote system")
+            traceback.print_exc()
+    else:
+        # TODO: Read from the DB file and push into ElasticSearch under the right index.
+        try:
+            connection = sqlite3.connect(source_file)
+            cursor = connection.cursor()
+            hosts = cursor.execute("select * from hosts")
+            for h in hosts:
+                print(h)
+        except:
+            # TODO: more granular exceptions
             traceback.print_exc()
 
 
@@ -42,16 +57,41 @@ def scp_files(host, source, destination):
     pass
 
 
-def setup_elastalert():
+def configure_elastalert():
     # TODO: Use for easy configuraiton of elastalert, filling in JSON template with provided information
     pass
+
+
+def setup_elasticsearch(workspace):
+    elastic = elasticsearch.Elasticsearch()
+    try:
+        mapping = {
+            "hosts": {
+                "properties": {
+                    "host": {"type": "string"},
+                    "ip_address": {"type": "string"},
+                    "region": {"type": "string"},
+                    "country": {"type": "string"},
+                    "latitude": {"type": "string"},
+                    "longitude": {"type": "string"},
+                    "module": {"type": "string"},
+                }
+            }
+        }
+
+        target_index = "recon_" + workspace
+        elastic.indices.create(target_index)
+        elastic.indices.put_mapping(index=target_index, doc_type="hosts", body=mapping)
+
+    except:
+        # TODO: more granular exceptions
+        traceback.print_exc()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Command line interface for ElastiBounty tools.")
     parser.add_argument('-i', help="Import data from sources: reconng, etc", dest='import_from')
-    parser.add_argument('-w', help="Workspace name. Must match workspace name from other tools, if applicable.",
-                        dest='workspace')
+    parser.add_argument('workspace', help="Workspace name. Must match workspace name from other tools, if applicable.")
     parser.add_argument('--remote',
                         help="Import data from a remote location. REMOTE location is a host configured in ssh config")
     opts = parser.parse_args()
