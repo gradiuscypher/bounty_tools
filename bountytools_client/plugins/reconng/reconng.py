b32bf8f6f54492cc3af2c93356412b40f307bb0f
gradiuscypher <gradiuscypher@gmail.com>
2018-02-19 12:55:19 -0800

    adding reconng work and some frontend attempt

--- bountytools_client/plugins/reconng/reconng.py
+++ bountytools_client/plugins/reconng/reconng.py
@@ -3,35 +3,35 @@
 
 import jsonrpclib
 import traceback
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 
 reconng = Blueprint('callback', __name__)
-
-
-# Setup the jsonrpclib for the recon-ng RPC server, stop the API if it cannot connect to the RPC server.
-try:
-    client = jsonrpclib.Server('http://localhost:4141')
-    sid = client.init()
-
-except:
-    print(traceback.format_exc())
-    print("The recon-ng RPC server was not started. Please start the RPC server before starting the BountyTools Client.")
-    exit(1)
 
 
 # Run a recon-ng module provided in the json POST
 @reconng.route('/run', methods=['POST'])
 def run():
+
+    # Setup the jsonrpclib for the recon-ng RPC server, stop the API if it cannot connect to the RPC server.
     try:
+        client = jsonrpclib.Server('http://localhost:4141')
+        sid = client.init()
+
+        # Get the configuration from JSON POST
         content = request.get_json()
         target_module = content['module']
-        return 'Success.', 200
+        target_domain = content['domain']
+        print(target_domain, target_module)
+
+        # Set the target domain
+        client.add('domains', target_domain, sid)
+        print(client.show('domains', sid))
+        client.use(target_module, sid)
+
+        # Execute the requested module and return the results
+        results = client.run(sid)
+
+        return jsonify(results)
+
     except:
-        print(traceback.format_exc())
-        return 'Fail.', 500
-
-
-# Returns the database with the recon-ng data
-@reconng.route('/get_database', methods=['GET'])
-def get_database():
-    pass
+        return traceback.format_exc(), 500
